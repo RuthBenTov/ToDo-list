6f624295b330aed6482e5b7038c89fc46fc2a78d
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2022-11-28 11:32:22 +0200

    upload files

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+let tasks = document.querySelectorAll("li")
+let binImg =  document.querySelector("img")
+
+tasks.forEach(t =>{
+    let delBtn = document.createElement("img")
+    delBtn.src = "/img/bin.png" 
+    t.appendChild(delBtn)
+    console.log("done!");
+    
+})
+
+let input = document.querySelector("input")!
+let addBtn = document.querySelector("#addBtn")!
+let binBtn = document.querySelector("img")
+addBtn.addEventListener("click",onSend)
+
+function onSend(){
+    if(input.value!=""){
+    let newTask = input.value!
+    let newLi = document.createElement("li")
+    let newSpan = document.createElement("span")
+    newSpan.textContent = newTask
+    newLi.appendChild(newSpan)
+    let newBtn =binBtn!.cloneNode(true)
+    newLi.appendChild(newBtn)
+    newBtn.addEventListener("click",()=>{
+    newBtn.parentElement!.remove()
+    })
+    let list = document.querySelector("ul")!
+    list.appendChild(newLi)
+    input.value =""
+    }
+}
+
+
+//remove with bin icon
+let bin = document.querySelectorAll("img")
+bin.forEach(b=>{
+    b.addEventListener("click",()=>{
+        b.parentElement!.remove()
+    })
+})
